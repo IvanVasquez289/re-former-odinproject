39379af3e3bbba8af426bd877ab1da7509e1e906
ivan <ivanjv1234@gmail.com>
2025-07-07 00:31:27 -0600

    feat: crud completed

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :users, only: [ :new, :create, :show, :edit, :update ]
+  root "users#new"
 end
